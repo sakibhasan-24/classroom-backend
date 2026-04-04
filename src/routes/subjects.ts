f791c2987dae6c633297d4c3c492e084afaed9dc
sakibhasan-24 <sakibhasan7724@gmail.com>
2026-04-04 22:00:19 +0600

    pagination logic with drizzle orm

--- src/routes/subjects.ts
+++ src/routes/subjects.ts
@@ -1,4 +1,6 @@
+import { ilike, or } from "drizzle-orm";
 import exrpress from "express"
+import { subjects } from "../db/schema";
 
 const router =exrpress.Router( )
 
@@ -8,8 +10,28 @@ const router =exrpress.Router( )
 
 router.get("/",(req,res)=>{
     try {
-        
+        const {search,department,page=1,limit=10}=req.query;
+        // pagination logic
+        const currentPage=Math.max(1,+page)
+        const limitPerPage=Math.max(1,+limit)
+        const offSet=(currentPage-1)*limitPerPage
+        // drizzle orm logic for filtering
+        const filterResult=[]
+        // if search exists
+        if(search){
+            filterResult.push(
+                or(ilike(subjects.name,`%${search}%`),(
+                    ilike(subjects.code,`%${search}%`)
+                ))
+            )
+        }
     } catch (error) {
         console.log(`Subjects not found ${error}`)
+        return res.json({
+            message:"Subjects not found",
+            success:false,
+            error:error,
+
+        })
     }
 })
